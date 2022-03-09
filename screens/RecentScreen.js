7a300f577c19be7c20bdeee64404f3136592a0e4
Tawan Wareeboot <aspirinehd111@gmail.com>
2022-03-09 16:43:31 +0700

    แก้ไขโครงสร้าง Screen v1.2
    แก้ไขโครงสร้าง Screen v1.2

--- screens/RecentScreen.js
+++ screens/RecentScreen.js
@@ -1,20 +1,42 @@
 import React from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
 
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
+
+function ThisMonth() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>This Month!</Text>
+    </View>
+  );
+}
+
+function LastMonth() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Last Month!</Text>
+    </View>
+  );
+}
+
+function NextMonth() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Next Month!</Text>
+    </View>
+  );
+}
+
 const RecentScreen = () => {
   return (
-    <View>
-      <View style={{flexDirection:'row', paddingTop:20, padding:50}}>
-        <TouchableOpacity>
-          <Text>Last Month</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>This Month</Text>
-        </TouchableOpacity>
-      </View>
-      <Text>Recent</Text>
-    </View>
-  )
+    <Tab.Navigator>
+      <Tab.Screen name="This Month" component={ThisMonth} />
+      <Tab.Screen name="Last Month" component={LastMonth} />
+      <Tab.Screen name="Next Month" component={NextMonth} />
+    </Tab.Navigator>
+  );
 }
 
 export default RecentScreen
